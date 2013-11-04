fb48cc4f3a1fd914d1e2400625c39196e4dbcc43
doubledouble <iamchenzewen@gmail>
2013-11-05 00:51:22 +0800

    by Iven

--- src/javacode/juc/BlockingQueneTest.java
+++ src/javacode/juc/BlockingQueneTest.java
@@ -1,7 +1,10 @@
 package javacode.juc;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.net.URLConnection;
 import java.nio.charset.Charset;
 import java.util.Scanner;
 import java.util.concurrent.ArrayBlockingQueue;
@@ -17,12 +20,13 @@ public class BlockingQueneTest {
           int FIlE_QUENE_SIZE  = 10;
           int SEARCH_THREADS  = 100;
           BlockingQueue<File> quene = new ArrayBlockingQueue<>(FIlE_QUENE_SIZE);
-          Scanner in = new Scanner(System.in);
-          System.out.println("请输入搜索文件路径");
+          Scanner in = new Scanner(System.in);   // windows GBK  ---> eclipse --> run as --> configurtion --> common --> GBK
+          System.out.println("请输入搜索文件路径");  // D:/tomcat_group/apache-tomcat-7.0.32_s1/logs
           String filePath = in.nextLine();
           System.out.println("请输入搜索词");
           String keyWord = in.nextLine();
           in.close();
+          System.out.printf("the filePath :%s, the keyWord : %s%n", filePath, keyWord);
           FileQuene fileQuene = new FileQuene(quene, new File(filePath));
           new Thread(fileQuene, "FileQuene Thread").start();
           for (int i=0; i<SEARCH_THREADS; i++) {
@@ -36,7 +40,7 @@ class FileQuene implements Runnable{
     
     private BlockingQueue<File> quene;
     private File startingDir;
-    public static final File dump = new File("");
+    public static final File DUMMY = new File("");
     
     public FileQuene(BlockingQueue<File> quene, File dir) {
         this.quene = quene;
@@ -45,16 +49,20 @@ class FileQuene implements Runnable{
 
     @Override
     public void run() {
-        extractFile(startingDir);
-        quene.offer(dump);
+        try {
+            extractFile(startingDir);
+            quene.put(DUMMY);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     
-    void extractFile(File dir) {
+    void extractFile(File dir) throws InterruptedException {
         for (File f : dir.listFiles()) {
             if (f.isDirectory()) {
                 extractFile(f);
             } else {
-                quene.offer(f);
+                quene.put(f);
             }
         }
     }
@@ -75,20 +83,35 @@ class Searcher implements Runnable {
     public void run() {
         boolean done = false;
         while (!done) {
-            File file = quene.poll();
-            if (file == FileQuene.dump) {
-                quene.offer(file);
-                done = true;
-            } else {
-                searchFile(file);
+            File file;
+            try {
+                file = quene.take();
+                if (file == FileQuene.DUMMY) { 
+                    quene.put(file);
+                    done = true;
+                } else {
+                    searchFile(file);
+                }
+            } catch (InterruptedException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
             }
         }
         
     }
     
-    void searchFile(File file) {
+    void searchFile(File file) throws IOException {
         try {
-            Scanner in = new Scanner(file); //new Scanner(new FileInputStream(file));
+//            System.out.println("scaning the file : " + file.getPath());
+            FileInputStream fs = new FileInputStream(file);
+//            String charSet = URLConnection.guessContentTypeFromStream(fs);
+//            if (charSet == null) 
+//                charSet = Charset.defaultCharset().name();
+            String charSet = "GBK";
+            Scanner in = new Scanner(fs, charSet); //new Scanner(new FileInputStream(file));
             int linenum = 0;
             while (in.hasNextLine()) {
                 String line = in.nextLine();
