859d218059faa4cf5d3054dc33d7cb2b7efbf35d
doubledouble <iamchenzewen@gmail>
2013-11-12 16:38:12 +0800

    Thread run方法结束后 nofityAll会被调用 持有该thread实例的线程会被唤醒

---
+++ src/iven/juc/AfterRunTest.java
@@ -0,0 +1,65 @@
+package iven.juc;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Thread run方法结束后 nofityAll会被调用 持有该thread实例的线程会被唤醒
+ * @author Administrator
+ *
+ */
+public class AfterRunTest {
+
+    /**
+     * @param args
+     * @throws InterruptedException 
+     */
+    public static void main(String[] args) throws InterruptedException {
+        Thread run = new AfterRun();
+        run.setName("obj-run-thread");
+        T t1 = new T(run, "wait-t1-thread");
+        T t2 = new T(run, "wait-t2-thread");
+        t1.start();
+        t2.start();
+        TimeUnit.SECONDS.sleep(1);
+        run.start();
+    }
+
+}
+
+class AfterRun extends Thread {
+
+    @Override
+    public void run() {
+        System.out.printf("==== before run current thread %s ====%n", Thread.currentThread().getName());
+        for (int i=0 ; i< 1000 * 1000; i++) {
+            
+        }
+        System.out.printf("==== after run current thread %s, the nofityAll is called ====%n", Thread.currentThread().getName());
+    }
+    
+}
+
+class T extends Thread {
+    
+    private Thread t;
+    
+    public T(Thread t, String name) {
+        this.t = t;
+        this.setName(name);
+    }
+
+    @Override
+    public void run() {
+        synchronized (t) {
+            try {
+                System.out.printf("==== before wait current name %s ====%n", Thread.currentThread().getName());
+                t.wait();
+                System.out.printf("==== after wait current name %s ====%n", Thread.currentThread().getName());
+            } catch (InterruptedException e) {
+                
+            }
+        }
+    }
+    
+}
+
--- src/iven/juc/ThreadStateTest.java
+++ src/iven/juc/ThreadStateTest.java
@@ -12,27 +12,56 @@ public class ThreadStateTest {
         T1 t1 = new T1();
         System.out.printf("==== before start state: %s====%n", t1.getState()); // NEW
         t1.start();
+        System.out.printf("==== t1 %s====%n", t1); // NEW
+        t1.myJoin();
+        t1.myJoin2();
         System.out.printf("==== running state: %s====%n", t1.getState()); //RUNNABLE
         
         TimeUnit.SECONDS.sleep(3);
         
         System.out.printf("==== running state: %s====%n", t1.getState()); // RUNNABLE
         System.out.printf("==== before interrupt isAlive: %s====%n", t1.isAlive()); // true
-        t1.interrupt();
-        TimeUnit.MILLISECONDS.sleep(1); // 中断延迟
-        System.out.printf("==== after interrupt state: %s====%n", t1.getState()); // TERMINATED
-        System.out.printf("==== after interrupt isAlive: %s====%n", t1.isAlive()); // false
-//        System.out.printf("==== after interrupt state: %s====%n", t1.getState()); // TERMINATED
+//        t1.interrupt();
+//        TimeUnit.MILLISECONDS.sleep(1); // 中断延迟
+            System.out.printf("==== after interrupt state: %s====%n", t1.getState()); // TERMINATED
+            System.out.printf("==== after interrupt isAlive: %s====%n", t1.isAlive()); // false
     }
 
 }
 
 class T1 extends Thread {
 
+    public synchronized void myJoin() throws InterruptedException {
+//        while (isAlive()) {
+            System.out.println("before my join");
+            System.out.println("before my join -- thread name " + Thread.currentThread().getName());            
+            System.out.println("this: " + this);
+            wait();
+            System.out.println("after my join is alive " + isAlive());
+            System.out.println("after my join");
+//        }
+    }
+    public void myJoin2() throws InterruptedException {
+//        while (isAlive()) {
+        synchronized (this) {
+            
+        System.out.println("before my myJoin2");
+        System.out.println("before my myJoin2 -- thread name " + Thread.currentThread().getName());            
+        System.out.println("this: " + this);
+        wait();
+        System.out.println("after my myJoin2 is alive " + isAlive());
+        System.out.println("after my myJoin2");
+//        }
+        }
+    }
+    
     @Override
     public void run() {
         while (!isInterrupted()) {
-            
+            for (int i=0; i < 10000; i++) {
+                System.out.println(i);
+            }
+            break;
         }
     }
     
